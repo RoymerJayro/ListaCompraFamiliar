1af5e8a6535984d6160d1961d5e35a72a763ab58
Roymer Jayro Pinto Morales <roymer.j.p.m@gmail.com>
2017-11-20 11:15:06 +0100

    Migración de las tablas

---
+++ server/boot/migration.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+'use strict';
+
+var config = require('../config.local.js')
+
+module.exports = function(app) {
+    //data sources
+    var mysqlDs = app.dataSources.mysqlDs;
+    
+    if(process.env.AUTOMIGRATE) {
+        mysqlDs.automigrate(null, function (err) {
+            if (err) console.log(err);
+            console.log('> Models migrated to tables');
+        });
+    }
+}
