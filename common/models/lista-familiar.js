eb13115bdb9b17417a473ba0e6d4db48941f1004
Roymer Jayro Pinto Morales <roymer.j.p.m@gmail.com>
2017-11-21 11:39:45 +0100

    Asgina al usuario la lista recién creada

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -11,5 +11,22 @@ module.exports = function(Listafamiliar) {
         context.args.data.owner = context.req.accessToken.userId;
         next();
     });
+    
+    Listafamiliar.afterRemote('create', function( context, listafamiliar, next) {
+        // Buscar el id de la lista
+        var idDeLaLista = listafamiliar.id
+        // Buscar al usuario con el id del accesstoken
+        var usuarioId = context.req.accessToken.userId;
+        var Usuario=Listafamiliar.app.models.Usuario;
+        Usuario.findById(usuarioId, function(err, usuario){
+       //asocia al usuario esa lista
+             usuario.listaFamiliarId = idDeLaLista;
+        //guardar el usuario
+             usuario.save(function(err){
+                 next();
+             });
+        });
+        
 
+    });
 };
