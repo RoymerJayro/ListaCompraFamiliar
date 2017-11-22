7be04a679a4a3ab2f45ee38c68cdb98b1ab63ea8
Roymer Jayro Pinto Morales <roymer.j.p.m@gmail.com>
2017-11-22 11:54:33 +0100

    Los usuarios autentificados pueden crear solicitud para entrar a una lista

--- common/models/lista-familiar.js
+++ common/models/lista-familiar.js
@@ -1,23 +1,23 @@
 'use strict';
 
-module.exports = function(Listafamiliar) {
+module.exports = function(ListaFamiliar) {
     
     
     
     
-    Listafamiliar.beforeRemote('create', function( context, listafamiliar, next) {
+    ListaFamiliar.beforeRemote('create', function( context, listafamiliar, next) {
         //var UserId=context.req.accessToken.userId;
         delete context.req.accessToken.userId;
         context.args.data.owner = context.req.accessToken.userId;
         next();
     });
     
-    Listafamiliar.afterRemote('create', function( context, listafamiliar, next) {
+    ListaFamiliar.afterRemote('create', function( context, listafamiliar, next) {
         // Buscar el id de la lista
-        var idDeLaLista = listafamiliar.id
+        var idDeLaLista = listafamiliar.id;
         // Buscar al usuario con el id del accesstoken
         var usuarioId = context.req.accessToken.userId;
-        var Usuario=Listafamiliar.app.models.Usuario;
+        var Usuario=ListaFamiliar.app.models.Usuario;
         Usuario.findById(usuarioId, function(err, usuario){
        //asocia al usuario esa lista
              usuario.listaFamiliarId = idDeLaLista;
@@ -26,7 +26,30 @@ module.exports = function(Listafamiliar) {
                  next();
              });
         });
-        
-
     });
+    
+/**
+ * Enlasaremos el usuario con su solicitud
+ * @param {object} context Contiene el context
+ * @param {Function(Error, object)} callback
+ */
+
+ListaFamiliar.prototype.solicitar = function(context, callback) {
+  var solicitud;
+  var userId=context.req.accessToken.userId;
+  var listafamiliar=this;
+  listafamiliar.solicitudes.add(userId, function(err) {
+            if(err)callback(err);
+            solicitud={
+                listafamiliarId:listafamiliar.id,
+                usuarioId:userId
+            }
+   });
+  callback(null, solicitud);
 };
+
+
+
+
+};
+
